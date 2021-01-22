a913f75188bf2480c59e10d208a01cc433de5fe5
eclipse_yjh <eclipse_yjh@qq.com>
2021-01-22 15:18:06 +0800

    My first commit in eclipse

---
+++ src/main/java/com/example/pojo/Apples.java
@@ -0,0 +1,11 @@
+package com.example.pojo;
+
+public class Apples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("杨键弘吃苹果");
+			System.out.println("杨键弘吃香蕉");
+	}
+
+}
